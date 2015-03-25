745674e7cb6d54d2a40c4a5379ee5915260593d8
Meng-Hsin Tung <itswindtw@gmail.com>
2015-03-25 14:30:57 -0400

    Completed events update logic in event page

--- background.js
+++ background.js
@@ -1,39 +1,94 @@
-/* Code related to backend */
 var SERVER_ENDPOINT = "http://localhost:9292";
 
-function requestEvents(callback) {
+function requestEvents(args) {
   var xhr = new XMLHttpRequest();
+  var url = SERVER_ENDPOINT + "/events";
 
-  xhr.open("GET", SERVER_ENDPOINT + "/events", true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.responseText) {
-      callback(JSON.parse(xhr.responseText));
+  if (args.start_at != undefined) {
+    url = url + "?start_at=" + args.start_at;
+  }
+
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      if (xhr.responseText) {
+        args.success(xhr, JSON.parse(xhr.responseText));
+      } else if (args.error) {
+        args.error(xhr);
+      } else {
+        console.error("requestEvents");
+      }
     }
   }
+
   xhr.send();
 }
 
 /* Extension code */
 
+var unread_count = 0;
+
 function onInit() {
-  // TODO: set up cursor
-
-  requestEvents(function (resp) {
-
-
+  chrome.storage.local.set({ "monitoring": {} });
+  requestEvents({
+    success: function (xhr, events) {
+      chrome.storage.local.set({"last_event_id": events.paging.last});
+    }
   });
-}
 
-function refreshEvents() {
   scheduleUpdate();
-
-  // TODO: fetch backend's events
-  //       if changed, budge icon and send update event to popup.html
 }
 
 function scheduleUpdate() {
-  chrome.alarms.create('refresh', { periodInMinutes: 5.0 });
+  chrome.alarms.create('refresh', { periodInMinutes: 1.0 });
 }
+
+
+function refreshEvents() {
+  // TODO: fetch backend's events
+  //       if changed, budge icon and send update event to popup.html
+  console.log('refreshEvents');
+
+  chrome.storage.local.get(["last_event_id", "monitoring"], function (items) {
+    requestEvents({
+      start_at: items.last_event_id,
+      success: function (xhr, events) {
+        if (events.paging.last != items.last_event_id) {
+          var changed_regions = [];
+
+          for (var i = events.data.length-1; i >= 0; --i) {
+            if (items.monitoring[events[i].url]) {
+              var monitoring_regions = items.monitoring[events[i].url];
+              var region_idx = monitoring_regions.indexOf(events[i].index);
+
+              if (region_idx != -1 &&
+                monitoring_regions[region_idx].hash_val != events[i].hash_val) {
+                changed_regions.push(events[i]);
+              }
+            }
+          }
+
+          unread_count += changed_regions.length;
+          chrome.browserAction.setBadgeText({text: unread_count.toString()});
+          console.log(changed_regions);
+
+          // TODO: publish changed events to popup
+
+          scheduleUpdate();
+
+          chrome.storage.local.set({"last_event_id": events.paging.end}, function () {
+            if (events.paging.end != events.paging.last) {
+              refreshEvents();
+            }
+          });
+        } else {
+          console.log('no change.');
+        }
+      }
+    });
+  });
+}
+
 
 function onAlarm(alarm) {
   refreshEvents();
@@ -47,5 +102,5 @@ chrome.runtime.onMessage.addListener(
     //   update region event
   });
 
-chrome.runtime.onInstalled.addListener(onAlarm);
+chrome.runtime.onInstalled.addListener(onInit);
 chrome.alarms.onAlarm.addListener(onAlarm);
